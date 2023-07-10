348e67a3d4742c5966e097790fa6223ce9dcc8d3
eliabn <30064396@tafe.wa.edu.au>
2023-07-10 11:55:50 +0800

    Add buttons and click event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,64 @@
 var startBtn = document.querySelector("#start");
 var h1 = document.querySelector("h1");
+var buttonContainer = document.querySelector('#button-container');
 
-var q1 = {
+var queNo = 0;
+var score = 0;
+var highScore = [];
+
+console.log(score)
+
+
+var allQ = [{
 question: "What is an a array in JavaScript",
 answers: ["Collect of datatypes", "Stores only strings", "A type of syntax", "Variable in an Oject"],
 correctAnswer: "Collect of datatypes",
-};
+}, {
+    question: "What is an a array in JavaScript",
+    answers: ["Collect of datatypes", "Stores only strings", "A type of syntax", "Variable in an Oject"],
+    correctAnswer: "Collect of datatypes",
+    }];
 
 function pickAnswer() {
-
+    var quizQ = allQ[queNo] 
     console.log("Hello");
-    h1.textContent = q1.question;
+    h1.textContent = quizQ.question;
+    var qNo = 1; 
     
+    // Click event handler
+    function checkAnswer(event) {
+        var selectedAnswer = event.target.textContent.substring(2)
+
+        console.log(selectedAnswer);
+        
+        if (selectedAnswer === quizQ.correctAnswer) {
+          console.log('Correct!');
+          score = score + 10;
+        } else {
+          console.log('Incorrect!');
+          score = score - 5;
+        }
+        console.log(score);
+    }
+
+    for (var i = 0; i < quizQ.answers.length; i++) {
+
+        var button = document.createElement('button');
+        button.textContent = qNo + " " + quizQ.answers[i];
+        console.log("Buttons");
+        qNo++
+        button.addEventListener('click', checkAnswer);
+        buttonContainer.appendChild(button);
+    }
+
+    function saveUser() {
+        var name = [{name:"James", score: 94}]
+        point = JSON.stringify(point)
+        localStorage.setItem("score", point);
+    }
+
 }
+
 
 
 startBtn.addEventListener("click", pickAnswer);
