8f5f055e485f207f43b9005c1da81a112bec333a
eliabn <30064396@tafe.wa.edu.au>
2023-07-11 17:35:00 +0800

    Finish quiz function and form for name entry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 var startBtn = document.querySelector("#start");
 var h1 = document.querySelector("h1");
 var buttonContainer = document.querySelector('#button-container');
+var form = 'hgh'
 
 var quesNo = 0;
 var score = 0;
-var highScore = [];
+//var highScore = [];
 
 console.log(quesNo)
 
@@ -25,26 +26,24 @@ var allQ = [
         answers: ["Collect of datatypes", "CCCCCCCCC", "CCCCCCCCC", "CCCCCCCCC"],
         correctAnswer: "Collect of datatypes",
     }];
-var quizQ = allQ[quesNo] 
-function startQuiz() {
     
-    console.log("Hello");
-    
-    h1.textContent = quizQ.question;
+    var quizQ = allQ[quesNo]
 
-    createButtons()
-    //removeButtons()
-
-    function saveUser() {
-        var name = "James"
-        point = JSON.stringify(point)
-        localStorage.setItem("score", point);
+    function startQuiz() {
+        console.log("Hello");
+      
+        h1.setAttribute("style", "text-align: left;");
+        document.querySelector('.card-contents').style.textAlign = 'left';
+      
+        h1.textContent = quizQ.question;
+      
+        createButtons();
+      
+        console.log(quesNo);
+      
+        startBtn.remove(); // Remove the start button from the DOM
     }
-    console.log(quesNo)
-    
-    startBtn.remove(); // Remove the start button from the DOM
 
-}
      
      
     // Click event handler
@@ -63,7 +62,8 @@ function startQuiz() {
       
         removeButtons();
         nextQuestion(); // Call nextQuestion() only once to move to the next question
-      }
+    }
+    
     
     
     // var buttonContainer = document.querySelector('#button-container');
@@ -71,7 +71,6 @@ function startQuiz() {
     
     function createButtons() {
         
-
         var qNo = 1;
         for (var i = 0; i < quizQ.answers.length; i++) {
             var button = document.createElement('button');
@@ -93,19 +92,73 @@ function startQuiz() {
     }
 
 
-    function nextQuestion() {
-        if (quesNo < allQ.length) {
-            quesNo++
-          quizQ = allQ[quesNo];
-          h1.textContent = quizQ.question;
-          removeButtons();
-          createButtons();
-          console.log("New Q" + quesNo + "___" + quizQ.question);
-        } else {
-            console.log("Quiz Completed")
-            h1.textContent = "Quiz Completed";
-        }
-    }
     
+
+    //removeButtons()
+    var highScore  = []
+    var vvvv = localStorage.getItem("highScore")
+
+
+
+function saveUser() {
+    var nameInput = document.getElementById("nameInput");
+    var name = nameInput.value;
+  
+    var score = 100; // Assuming you have a score value
+  
+    // Add the user's name and score to the highScore array
+    highScore.push({ name: name, score: score });
+  
+    // Convert the highScore array to JSON and store it in local storage
+    localStorage.setItem("highScore", JSON.stringify(highScore));
+  }
+  
+  
+  function createNameInput() {
+    var cardContents = document.querySelector('.card-contents'); // Get the .card-contents element
+  
+    var nameForm = document.createElement("form");
+    nameForm.id = "nameForm";
+  
+    var nameLabel = document.createElement("label");
+    nameLabel.textContent = "Enter your name:";
+    nameLabel.setAttribute("for", "nameInput");
+  
+    var nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.id = "nameInput";
+    nameInput.required = true;
+  
+    var submitButton = document.createElement("button");
+    submitButton.type = "submit";
+    submitButton.textContent = "Submit";
+  
+    nameForm.appendChild(nameLabel);
+    nameForm.appendChild(nameInput);
+    nameForm.appendChild(submitButton);
+  
+    cardContents.appendChild(nameForm); // Append the form to the .card-contents element
+  }
+  
+  // ...
+  
+  function nextQuestion() {
+    quesNo++;
+    if (quesNo < allQ.length) {
+      quizQ = allQ[quesNo];
+      h1.textContent = quizQ.question;
+      removeButtons();
+      createButtons();
+      console.log("New Q" + quesNo + "___" + quizQ.question);
+    } else {
+      console.log("Quiz Completed");
+      h1.textContent = "Quiz Completed";
+      var scoreText = document.createElement("p");
+      scoreText.textContent = "Your final score is " + score;
+      buttonContainer.appendChild(scoreText);
+      createNameInput();
+    }
+  }
+  
 
 startBtn.addEventListener("click", startQuiz);
